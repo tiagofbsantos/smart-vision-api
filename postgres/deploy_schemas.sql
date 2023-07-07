81ae99184ab7e77f4afa1ebe1e3c237b90bb3c78
tiagofbsantos <santosfbtiago@gmail.com>
2023-07-07 20:11:12 +0200

    updated sql files.

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,5 +1,5 @@
 -- Deploy fresh database tables
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
-\i '/docker-entrypoint-initdb.d/tables/login.sql'
+\i '/docker-entrypoint-initdb.d/tables/credentials.sql'
 
 \i '/docker-entrypoint-initdb.d/seed/seed.sql'
